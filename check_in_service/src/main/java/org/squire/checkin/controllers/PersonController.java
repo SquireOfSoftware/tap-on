dadc8014ce0cfbbc9d0da7a99da2bf2094c60038
Joseph Tran <fred.smoth@gmail.com>
2018-06-11 16:15:23 +1000

    Adding in CORS support

--- check_in_service/src/main/java/org/squire/checkin/controllers/PersonController.java
+++ check_in_service/src/main/java/org/squire/checkin/controllers/PersonController.java
@@ -3,6 +3,7 @@ package org.squire.checkin.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.squire.checkin.services.SignInTimeService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class PersonController {
     private PersonService personService;
