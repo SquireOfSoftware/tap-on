fcd7e860ead3faaa4d263286f1d321b67db91acc
Joseph Tran <fred.smoth@gmail.com>
2019-09-02 11:18:15 +1000

    Adding in a spring boot application

---
+++ people-service/src/main/java/com/squireofsoftware/peopleproject/PeopleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.squireofsoftware.peopleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleProjectApplication.class, args);
+	}
+
+}
